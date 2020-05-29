fdcaacd3823f20eb3d5679f9948eaa5b78c35116
jimtanvir <tanvirrahman117@gmail.com>
2020-05-30 03:07:55 +0600

    final update complete

--- app.js
+++ app.js
@@ -1,11 +1,76 @@
 const products = document.querySelector('.products');
-const cartContainer = document.querySelector('.cart-items-container')
+const cartContainer = document.querySelector('.cart-items-container');
+const cart = document.querySelector('.cart');
+const cartStatus = document.querySelector('.cart-status');
+const allPrice = document.querySelector('.allprice');
+
+// if(Array.prototype.slice.call(cartContainer.children)){
+
+// }
+
+
+
+
+
+
+const cartEmptyCheck = () =>{
+    if(Array.prototype.slice.call(cartContainer.children).length < 2){
+        cartContainer.style.display = 'none';
+        cart.classList.add('empty-cart');
+        cart.children[0].style.display = 'block';
+        cart.children[2].style.display = 'none';
+    }
+    
+    else {
+        cartContainer.style.display = 'block';
+        cart.classList.remove('empty-cart');
+        cart.children[0].style.display = 'none';
+        cart.children[2].style.display = 'block';
+    }
+}
+
+
+cartEmptyCheck();
+
+
+
+const allTotal = () =>{
+    const array_Of_Cart_Items = Array.prototype.slice.call(cartContainer.children);
+    let totalPrice = 0;
+    let totalQuantity = 0;
+ array_Of_Cart_Items.forEach(element => {
+    
+  
+
+    if(element.children[0].innerText !== 'Name'){
+
+    
+   
+    let quantity = parseFloat(element.children[2].innerText);
+    totalQuantity += quantity; 
+
+
+
+
+     let total = parseFloat(element.children[3].innerText);
+     totalPrice +=  total;
+    
+    }
+
+    
+    
+    });
+    cartStatus.children[1].innerText = totalQuantity;
+    allPrice.innerText = totalPrice;
+}
+
 
 
 const addToCart = (name,price) => {
 
 
- const array_Of_Cart_Items = Array.prototype.slice.call(cartContainer.children );
+const array_Of_Cart_Items = Array.prototype.slice.call(cartContainer.children);
+
 
  let exist = false;
  
@@ -39,7 +104,11 @@ const addToCart = (name,price) => {
     }
 
 
+    
 
+    allTotal();
+
+    cartEmptyCheck();
 
 }
 
@@ -100,5 +169,8 @@ cartContainer.addEventListener('click',(e)=>{
         if(e.target.classList[0] === 'cancel'){
             e.target.parentNode.parentNode.remove();
         }
+
+        allTotal();
+        cartEmptyCheck();
 })
 
